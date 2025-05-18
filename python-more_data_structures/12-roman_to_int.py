99cd1f6f3b567e3dffa314fec1cb940d32352093
“vernfleming” <10568@holbertonstudents.com>
2025-05-18 18:00:07 +1000

    12 fix 1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,12 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
     total = 0
     prev = 0
     for ch in reversed(roman_string):
