84e3590db7bf0b5a9d37463c0c3eb9a2feaba8f2
“vernfleming” <10568@holbertonstudents.com>
2025-05-18 20:52:26 +1000

    various fixes

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,8 +2,10 @@
 if __name__ == "__main__":
     import sys
 
+    num_args = len(sys.argv) - 1
     total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
-    print(total)
-    
+
+    for i in range(num_args):
+        total += int(sys.argv[i + 1])
+
+print(f"{total}")
