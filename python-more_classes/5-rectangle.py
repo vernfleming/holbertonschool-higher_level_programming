5551723ceef10e7f8d27b28775662a645c39ab0a
“vernfleming” <10568@holbertonstudents.com>
2025-05-25 22:03:54 +1000

    5 fix 2

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle with full functionality and deletion message."""
+"""Defines a class Rectangle with full
+functionality and deletion message."""
 
 
 class Rectangle:
@@ -49,13 +50,13 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle using '#' characters."""
+        """Returns a string of the rectangle using '#' characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Returns a reproducible string representation of the rectangle."""
+        """Returns a reproducible string of the rectangle."""
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
