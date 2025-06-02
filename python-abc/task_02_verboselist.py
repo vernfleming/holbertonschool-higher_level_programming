dcae3837e3f9fd1f38ba94ef2a468a1cbba1d451
“vernfleming” <10568@holbertonstudents.com>
2025-06-02 23:47:45 +1000

    project work

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
